124736aeab26e7d4b232542f575906cc209593c7
Jan Jörka <jan.jorka@seznam.cz>
2023-03-22 00:11:18 +0100

    Improved sign in page

--- client/projects/application/src/app/user/presentation/pages/sign-in-page/sign-in-page.component.ts
+++ client/projects/application/src/app/user/presentation/pages/sign-in-page/sign-in-page.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 import { FirebaseUIModule } from 'firebaseui-angular';
+import { PageComponent } from 'projects/application/src/app/shared/presentation/components/page/page.component';
 
 @Component({
     standalone: true,
     selector: 'app-sign-in-page',
-    imports: [FirebaseUIModule, MatIconModule],
+    imports: [FirebaseUIModule, MatIconModule, PageComponent, MatCardModule],
     templateUrl: './sign-in-page.component.html',
     styleUrls: ['./sign-in-page.component.css']
 })
 export class SignInPageComponent {
+    constructor(private readonly router: Router) { }
 
+    onSignInSuccess() {
+        this.router.navigateByUrl("/projects");
+    }
 }
