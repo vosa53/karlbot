1b7b8bd14e3a2fc3a3af651d2f1aac56b0556d9c
Jan Jörka <jan.jorka@seznam.cz>
2023-05-05 17:16:14 +0200

    Added 404 error handling

--- client/projects/application/src/app/shared/application/error-handling/application-error-handler.ts
+++ client/projects/application/src/app/shared/application/error-handling/application-error-handler.ts
@@ -1,23 +1,29 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { ErrorHandler, Injectable, isDevMode, NgZone } from "@angular/core";
+import { Router } from "@angular/router";
 import { DialogService } from "../../presentation/services/dialog.service";
 import { NotificationService } from "../../presentation/services/notification.service";
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
     constructor(
+        private readonly router: Router,
         private readonly notificationService: NotificationService, 
-        private readonly dialogService: DialogService, 
+        private readonly dialogService: DialogService,
         private readonly zone: NgZone
     ) {
         super();
     }
 
     override handleError(error: any) {
+        if (this.isHTTPError(error, 404)) {
+            this.router.navigateByUrl("/not-found", { skipLocationChange: true });
+            return;
+        }
+
         super.handleError(error);
 
         const displayMessage = this.getDisplayMessage(error);
-
         this.zone.run(() => {
             this.notificationService.show(displayMessage);
             if (isDevMode())
@@ -26,14 +32,14 @@ export class ApplicationErrorHandler extends ErrorHandler {
     }
 
     private getDisplayMessage(error: any): string {
-        if (this.isNetworkError(error))
+        if (this.isHTTPError(error, 0))
             return "Network error! Please check if the internet connection is available.";
 
         return "Something went wrong!";
     }
 
-    private isNetworkError(error: any): boolean {
-        return error instanceof HttpErrorResponse && error.status === 0 ||
-            error?.rejection instanceof HttpErrorResponse && error?.rejection.status === 0
+    private isHTTPError(error: any, statusCode: number): boolean {
+        return error instanceof HttpErrorResponse && error.status === statusCode ||
+            error?.rejection instanceof HttpErrorResponse && error?.rejection.status === statusCode;
     }
 }
